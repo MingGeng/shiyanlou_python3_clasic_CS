dec0ba222968471de66a23fead5d0e02a33751ed
MingGeng <gengming8859@icloud.com>
2020-10-11 10:33:45 +0800

    add .gitignore

--- myChapter1/fib1.py
+++ myChapter1/fib1.py
@@ -1,7 +1,8 @@
 import sys
-from typing import Dict, Generator
-from functools import lru_cache
+from collections import defaultdict
 from datetime import datetime
+from functools import lru_cache
+from typing import Dict, Generator
 
 gcount_fib1 = 0
 gcount_fib3 = 0
@@ -9,9 +10,10 @@ gcount_fib4 = 0
 gcount_fib5 = 0
 gcount_fib6 = 0
 
-
+total = defaultdict(int)
 
 memo: Dict[int, int] = {0: 0, 1: 1}
+
 
 def fib1(n: int) -> int:
     global gcount_fib1
@@ -23,16 +25,17 @@ def fib1(n: int) -> int:
 
     return fib1(n - 1) + fib1(n - 2)
 
+
 def fib3(n: int) -> int:
     global gcount_fib3
     gcount_fib3 += 1
-
 
     if n not in memo:
         memo[n] = fib3(n - 1) + fib3(n - 2)
 
     print("fib3 total: ", gcount_fib3)
     return memo[n]
+
 
 @lru_cache(maxsize=None)
 def fib4(n: int) -> int:
@@ -43,7 +46,7 @@ def fib4(n: int) -> int:
         return n
 
     print("fib4 total: ", gcount_fib4)
-    return fib4(n -1) + fib4(n -2)
+    return fib4(n - 1) + fib4(n - 2)
 
 
 def fib5(n: int) -> int:
@@ -72,34 +75,45 @@ def fib6(n: int) -> Generator[int, None, None]:
     for _ in range(1, n):
         last, next = next, last + next
         gcount_fib6 += 1
-        print("fib6(%d): %d" %(gcount_fib6+1,next))
+        print("fib6(%d): %d" % (gcount_fib6 + 1, next))
         return next
         # yield next
+
+
+def fib7(k):
+    assert k > 0, 'K must larger than 0'
+    if k in [1, 2]:
+        return 1
+    global total
+    total[k] += 1
+    return fib7(k - 1) + fib7(k - 2)
 
 
 if __name__ == "__main__":
     start_time = datetime.now()
     print("fib1(20):  ", fib1(int(20)))
-    print("耗时: {}".format((datetime.now()-start_time).total_seconds()))
+    print("耗时: {}".format((datetime.now() - start_time).total_seconds()))
     print("=======================")
     start_time = datetime.now()
 
-    print("fib3: ",fib3(int(sys.argv[1])))
-    print("耗时: {}".format((datetime.now()-start_time).total_seconds()))
+    print("fib3: ", fib3(int(sys.argv[1])))
+    print("耗时: {}".format((datetime.now() - start_time).total_seconds()))
     start_time = datetime.now()
 
-    print("fib4 ",fib4(int(sys.argv[1])))
-    print("耗时: {}".format((datetime.now()-start_time).total_seconds()))
+    print("fib4 ", fib4(int(sys.argv[1])))
+    print("耗时: {}".format((datetime.now() - start_time).total_seconds()))
     start_time = datetime.now()
 
-    print("fib5 ",fib5(int(sys.argv[1])))
-    print("耗时: {}".format((datetime.now()-start_time).total_seconds()))
+    print("fib5 ", fib5(int(sys.argv[1])))
+    print("耗时: {}".format((datetime.now() - start_time).total_seconds()))
     start_time = datetime.now()
 
-    print("fib6 ",fib6(int(sys.argv[1])))
-    print("耗时: {}".format((datetime.now()-start_time).total_seconds()))
+    # print("fib6 ",fib6(int(sys.argv[1])))
+    # print("耗时: {}".format((datetime.now()-start_time).total_seconds()))
 
     # for i in fib6(int(sys.argv[1])):
     #     print("fib6:", i)
-        # pass
+    # pass
+    print("fib7(20): {}".format(fib7(20)))
+    print(total)
 
